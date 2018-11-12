41564750046ea9f4ea262ad449b85e53043a24c0
Melissa Gayton <melissa@Melissas-MacBook-Pro-3.local>
2018-11-12 14:25:21 -0500

    data downloaded

--- scratch.R
+++ scratch.R
@@ -1,3 +1,6 @@
+library(fs)
+library(tidyverse)
+
 # download upshot elections data
 
 download.file(url = "https://goo.gl/ZRCBda",
@@ -8,3 +11,18 @@ download.file(url = "https://goo.gl/ZRCBda",
 unzip("master.zip")
 
 file_delete("master.zip")
+
+# read context data
+
+election_context <- read_csv("https://raw.githubusercontent.com/MEDSL/2018-elections/master/election-context-2018.csv")
+
+# read all the upshot data identified by source
+
+file_names <- dir_ls("2018-live-poll-results-master/data/")
+all <- map_dfr(file_names, read_csv, .id  = "source")
+
+# get just house data
+
+house <- all %>%
+  filter(grepl("sen", source) == FALSE, grepl("gov", source) == FALSE) 
+
