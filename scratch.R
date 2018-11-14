aeebc1bd0bf26a765e75f0346363cefa8717cbc5
Melissa Gayton <melissa@Melissas-MacBook-Pro-3.local>
2018-11-13 22:49:28 -0500

    copied midterm code

--- scratch.R
+++ scratch.R
@@ -23,6 +23,120 @@ all <- map_dfr(file_names, read_csv, .id  = "source")
 
 # get just house data
 
-house <- all %>%
+total_house_data <- all %>%
   filter(grepl("sen", source) == FALSE, grepl("gov", source) == FALSE) 
 
+# extract just the district from list of file names
+districts <- str_sub(file_names, start= -10, end = -7)
+# duplicated gives true false list on if there's a duplicate
+dupes_list <- duplicated(districts)
+# prepare a list to place duplicated districts in
+dupes_data <- list()
+
+# prepare counters
+i <- 1
+j <- 1
+# for each district
+for(i in 1:length(districts)) {
+  # if theres a duplicate
+  if(dupes_list[[i]] == TRUE) {
+    # add it to the list of dupes
+    dupes_data[[j]] <- districts[[i]]
+    # add 1 to list counter so next duplicate goes in the next spot on the list
+    j <- j + 1
+  }
+  # go to the next district
+  i <- i + 1
+}
+
+# get the last poll for duplicated districts
+
+dupes <- total_house_data %>%
+  mutate(district = str_sub(source, start= -10, end = -7)) %>%
+  filter(district %in% dupes_data) %>%
+  select(district, response, final_weight, source) %>%
+  mutate(poll = str_sub(source, start= -5, end = -5))
+
+# change 2 to 1 because in pa, first poll was in second wave (no other second waves)
+dupes$poll[dupes$poll == "2" | dupes$poll == "1"] <- "Poll 1"
+# change 3 to 2 to represent that its the 2nd poll in that district
+dupes$poll[dupes$poll == "3"] <- "Poll 2"
+
+dupes <- dupes %>%
+  filter(response == "Rep" | response == "Dem", poll == "Poll 2") %>%
+  select(district, response, final_weight)
+
+# get nonduplicated districts
+
+no_dupes <- total_house_data %>%
+  mutate(district = str_sub(source, start= -10, end = -7)) %>%
+  filter(! district %in% dupes_data) %>%
+  select(district, response, final_weight)
+
+# combine duplicated house polls with nonduplicated house polls then also with governor and senate polls
+pre <- merge(dupes, no_dupes, all = TRUE)
+
+# calculate the rep adv for all the polls
+pre <- pre %>%
+  # calculate adv
+  group_by(district) %>%
+  mutate(total = sum(final_weight)) %>%
+  group_by(response, district, total) %>%
+  tally(wt = final_weight) %>%
+  spread(response, n) %>%
+  group_by(district) %>%
+  mutate(rep_adv = (Rep - Dem) / total) %>%
+  # make district pretty
+  select(district, rep_adv) %>%
+  ungroup() %>%
+  mutate(district = toupper(district)) %>%
+  mutate(district = paste(district = substr(district, 1, 2), "-", substr(district, 3, nchar(district)), sep = ""))
+
+remove_zero <- function(district_code) {
+  district_code <- gsub("-0", "-", district_code)
+  return(district_code)
+}
+
+pre$district <- lapply(pre$district, remove_zero)
+
+# district changed into unknown type for some reason, change back to character
+
+pre <- pre %>%
+  mutate(district = as.character(district))
+
+# get the results data
+
+results_data <- read_csv("mt_2_results.csv")
+
+# change it to match formatting of other data
+
+# match senate and gov
+results_data$district[results_data$district == "Senate"] <- "SEN"
+results_data$district[results_data$district == "Governor"] <- "GOV"
+
+# match district variable
+
+results <- results_data %>%
+  mutate(district = paste(0, district, sep = "")) %>%
+  select(state, district, dem_votes, rep_votes)
+
+results <- q4_results %>%
+  unite("district", state, district, sep = "-", remove = FALSE)
+
+results$district <- lapply(results$district, remove_zero)
+
+# district changed into unknown type for some reason, change back to character
+
+results <- results %>%
+  mutate(district = as.character(district))
+
+# join the poll data and results data
+
+combo <- left_join(pre, results, by = "district")
+
+# calculate the actual rep adv
+
+combo <- combo %>%
+  mutate(actual = (rep_votes - dem_votes) / (rep_votes + dem_votes)) %>%
+  select(district, rep_adv, actual, state) %>%
+  adorn_pct_formatting()
